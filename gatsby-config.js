e5efe67b714c01f782ddeb04dddeecac6c3fff97
zaffre001 <zaffre001@gmail.com>
2020-03-30 00:25:45 +0900

    이미지 플러그인 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,46 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "zaffre log",
+    author: "heejeong park",
+    social: [
+      {
+        name: "github",
+        url: "https://github.com/zaffre001",
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          }],
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
+    {
+      resolve:`gatsby-transformer-remark`,
+      options:{
+
+      }
+    },
+    `gatsby-transformer-sharp`,
+  ],
 }
